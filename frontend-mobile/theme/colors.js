ec4eaa4ccffd6104dc2fd21a5e97d53d3be91383
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-04-09 12:21:24 +0200

    Added file with theme colors

---
+++ frontend-mobile/theme/colors.js
@@ -0,0 +1,19 @@
+export const lightTheme = {
+  // Grundfärger
+  primary: "#A1A774",
+  secondary: "#FFFFFF",
+  accent: "#696D46",
+
+  // Layout och bakgrunder
+  background: "#A1A774",
+  card: "#FFFFFF",
+  headerBackground: "#A1A774",
+  tabBackground: "#A1A774",
+  lightButton: "#FFDE8D",
+  darkButton: "#4A342B",
+  neutralButton: "#696D46",
+
+  // Text
+  textPrimary: "#000000",
+  textSecondary: "#FFFFFF",
+};
