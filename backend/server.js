c79520d585b7ad2797cab958bf989ba6939592c5
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-04-11 09:19:29 +0200

    add get api endpoints

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,19 @@ const router = 3000;
 // Middleware för konvertera till json data
 app.use(express.json());
 
+let databaseFlowers = [];
+
+//
+app.get('/', (req, res) => {
+  res.json('Server is live');
+});
+
+// Hämta flower databas
+app.get('/api/get-flowers', (req, res) => {
+  res.json(databaseFlowers);
+});
+
+// Start server
 app.listen(router, () => {
   console.log(`Server is live http://localhost:${router}/`);
 });
