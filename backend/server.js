4731243044efc966c938035e708ebb3d11a971aa
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-04-11 10:00:22 +0200

    add get api endpoint

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,14 @@
 import express from 'express';
 
 const app = express();
-const router = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware för konvertera till json data
 app.use(express.json());
 
 let databaseFlowers = [];
 
-//
+// Test endpoint för server debugging
 app.get('/', (req, res) => {
   res.json('Server is live');
 });
@@ -18,7 +18,27 @@ app.get('/api/get-flowers', (req, res) => {
   res.json(databaseFlowers);
 });
 
+// Lägg till blommor
+app.post('/api/add-flowers', (req, res) => {
+  const flowers = {
+    id: Date.now(),
+    name: req.body.name,
+    // specie: req.body.specie,
+    // moisture: req.body.moisture,
+    // temperature: req.body.temperature,
+    // humidity: req.body.humidity,
+  };
+
+  databaseFlowers.push(flowers);
+  res.json(databaseFlowers);
+});
+
+// Ta bort blommor
+app.delete('/api/remove-flower/:id', (req, res) => {
+  databaseFlowers = databaseFlowers.filter((flower) => flower.id !== id);
+});
+
 // Start server
-app.listen(router, () => {
-  console.log(`Server is live http://localhost:${router}/`);
+app.listen(PORT, () => {
+  console.log(`Server is live http://localhost:${PORT}/`);
 });
