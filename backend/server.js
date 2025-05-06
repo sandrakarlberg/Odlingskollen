ac44866b59f6c7e2257f736f3ebe5347904a1ec5
Jonatan <180638563+jont-ctrl@users.noreply.github.com>
2025-05-06 09:38:56 +0200

    import pool dotenv

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,19 @@
 import express from 'express';
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+});
 
 // Middleware för konvertera till json data
 app.use(express.json());
