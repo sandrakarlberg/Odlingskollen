48977b5321bfe66cb0798739aaad6f8d2d5b0680
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-05-20 21:28:08 +0200

    URL-config added

--- frontend-mobile/services/api.js
+++ frontend-mobile/services/api.js
@@ -1,9 +1,12 @@
 import { jwtDecode } from "jwt-decode";
+import { Platform } from "react-native";
+import * as SecureStore from "expo-secure-store";
+import baseUrl from "./urlConfig";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const fetchUsers = async () => {
   try {
-    const response = await fetch("http://localhost:3000/api/get-users");
+    const response = await fetch(`${baseUrl}/api/get-users`);
     if (!response.ok) throw new Error("Fel vid hämtning av användare.");
     return await response.json();
   } catch (error) {
@@ -14,7 +17,7 @@ export const fetchUsers = async () => {
 
 export const apiLogin = async (email, password) => {
   try {
-    const response = await fetch("http://localhost:3000/api/login", {
+    const response = await fetch(`${baseUrl}/api/login`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
@@ -25,7 +28,10 @@ export const apiLogin = async (email, password) => {
     const data = await response.json();
 
     if (response.ok) {
-      await AsyncStorage.setItem("token", data.token);
+      console.log("Received token:", data.token);
+      Platform.OS === "web"
+        ? await AsyncStorage.setItem("token", data.token)
+        : await SecureStore.setItemAsync("token", data.token);
       const decoded = jwtDecode(data.token);
       return { success: true, name: decoded.name, userId: decoded.userId };
     } else {
@@ -39,15 +45,15 @@ export const apiLogin = async (email, password) => {
 
 export const fetchPlants = async (userId) => {
   try {
-    const token = await AsyncStorage.getItem("token");
-    const response = await fetch(
-      `http://localhost:3000/api/${userId}/get-flowers`,
-      {
-        headers: {
-          Authorization: `Bearer ${token}`,
-        },
-      }
-    );
+    const token =
+      Platform.OS === "web"
+        ? await AsyncStorage.getItem("token")
+        : await SecureStore.getItemAsync("token");
+    const response = await fetch(`${baseUrl}/api/${userId}/get-flowers`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
     if (!response.ok) throw new Error("Fel vid hämtning av data.");
     return await response.json();
   } catch (error) {
@@ -58,8 +64,11 @@ export const fetchPlants = async (userId) => {
 
 export const addPlant = async (name, id) => {
   try {
-    const token = await AsyncStorage.getItem("token");
-    const response = await fetch(`http://localhost:3000/api/${id}/add-flower`, {
+    const token =
+      Platform.OS === "web"
+        ? await AsyncStorage.getItem("token")
+        : await SecureStore.getItemAsync("token");
+    const response = await fetch(`${baseUrl}/api/${id}/add-flower`, {
       method: "POST",
       headers: {
         Authorization: `Bearer ${token}`,
@@ -78,7 +87,7 @@ export const addPlant = async (name, id) => {
 
 export const addUser = async (name, password, email) => {
   try {
-    const response = await fetch("http://localhost:3000/api/create-users", {
+    const response = await fetch(`${baseUrl}/api/create-users`, {
       method: "POST",
       headers: {
         "Content-type": "application/json",
