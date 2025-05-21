45d728676f41bc2c1763e2e613d41234e9616b14
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-05-21 12:38:25 +0200

    Centered Timeline

--- frontend-mobile/components/Timelines.jsx
+++ frontend-mobile/components/Timelines.jsx
@@ -12,11 +12,13 @@ export default class Timelines extends Component {
         title: "Tomat 1",
         description: "Event 1 Description",
         icon: (
-          <View style={{
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 15,
-          }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 15,
+            }}
+          >
             <Image
               source={require("../assets/icons8-tomato-48.png")}
               style={{ width: 20, height: 20 }}
@@ -29,11 +31,13 @@ export default class Timelines extends Component {
         title: "Citron 2",
         description: "Event 2 Description",
         icon: (
-          <View style={{
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 15,
-          }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 15,
+            }}
+          >
             <Image
               source={require("../assets/icons8-citrus-48.png")}
               style={{ width: 20, height: 20 }}
@@ -46,11 +50,13 @@ export default class Timelines extends Component {
         title: "Tomat 3",
         description: "Event 3 Description",
         icon: (
-          <View style={{
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 15,
-          }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 15,
+            }}
+          >
             <Image
               source={require("../assets/icons8-tomato-48.png")}
               style={{ width: 20, height: 20 }}
@@ -63,11 +69,13 @@ export default class Timelines extends Component {
         title: "Citron 4",
         description: "Event 4 Description",
         icon: (
-          <View style={{
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 15,
-          }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 15,
+            }}
+          >
             <Image
               source={require("../assets/icons8-citrus-48.png")}
               style={{ width: 20, height: 20 }}
@@ -80,11 +88,13 @@ export default class Timelines extends Component {
         title: "Tomat 5",
         description: "Event 5 Description",
         icon: (
-          <View style={{
-            backgroundColor: "white",
-            padding: 4,
-            borderRadius: 15,
-          }}>
+          <View
+            style={{
+              backgroundColor: "white",
+              padding: 4,
+              borderRadius: 15,
+            }}
+          >
             <Image
               source={require("../assets/icons8-tomato-48.png")}
               style={{ width: 20, height: 20 }}
@@ -97,32 +107,34 @@ export default class Timelines extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Timeline
-          style={styles.list}
-          data={this.data}
-          circleSize={30}
-          circleColor={lightTheme.secondary}
-          circleStyle={{
-            border: lightTheme.primary,
-            borderWidth: 2,
-            padding: 5,
-          }}
-          lineColor={{ backgroundColor: lightTheme.neutral }}
-          timeContainerStyle={{ minWidth: 80, marginTop: 5 }}
-          timeStyle={{
-            textAlign: "center",
-            backgroundColor: lightTheme.primary,
-            color: "white",
-            padding: 5,
-            borderRadius: 4,
-          }}
-          descriptionStyle={{ color: "gray" }}
-          options={{
-            style: { paddingTop: 5 },
-          }}
-          innerCircle={"icon"}
-        />
+      <View style={{backgroundColor: "#FFFFFF"}}>
+        <View style={styles.container}>
+          <Timeline
+            style={styles.list}
+            data={this.data}
+            circleSize={30}
+            circleColor={lightTheme.secondary}
+            circleStyle={{
+              border: lightTheme.primary,
+              borderWidth: 2,
+              padding: 5,
+            }}
+            lineColor={{ backgroundColor: lightTheme.neutral }}
+            timeContainerStyle={{ minWidth: 80, marginTop: 5 }}
+            timeStyle={{
+              textAlign: "center",
+              backgroundColor: lightTheme.primary,
+              color: "white",
+              padding: 5,
+              borderRadius: 4,
+            }}
+            descriptionStyle={{ color: "gray" }}
+            options={{
+              style: { paddingTop: 5 },
+            }}
+            innerCircle={"icon"}
+          />
+        </View>
       </View>
     );
   }
@@ -133,9 +145,9 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: "row",
     padding: 20,
-    paddingTop: 65,
     backgroundColor: lightTheme.secondary,
-    marginBottom: 30,
+    marginBottom: 50,
+    margin: "auto",
   },
   list: {
     flex: 1,
