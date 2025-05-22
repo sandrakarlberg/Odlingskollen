527e1977ec1a77ee7bee1095824b69d925cbed91
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-05-22 10:38:10 +0200

    Fixed dashboard error

--- frontend-mobile/screens/Dashboard.jsx
+++ frontend-mobile/screens/Dashboard.jsx
@@ -1,26 +1,32 @@
-import { StyleSheet, View, SafeAreaView, ScrollView } from "react-native";
-import React from "react";
+import { FlatList, SafeAreaView, StyleSheet } from "react-native";
 import DashboardHeader from "../components/DashboardHeader";
 import InfoCardSection from "../components/InfoCardSection";
 import SensorStatus from "../components/SensorStatus";
 import { lightTheme } from "../theme/colors";
 import { useUser } from "../context/UserContext";
-import Timeline from "../components/Timelines";
+import Timelines from "../components/Timelines";
 
 const Dashboard = () => {
   const { username } = useUser();
 
   return (
     <SafeAreaView style={styles.container}>
-      <ScrollView>
-      <DashboardHeader
-        url={require("../assets/icon-farmer-2.png")}
-        user={username}
+      <FlatList
+        ListHeaderComponent={
+          <>
+            <DashboardHeader
+              url={require("../assets/icon-farmer-2.png")}
+              user={username}
+            />
+            <InfoCardSection />
+            <SensorStatus />
+            <Timelines />
+          </>
+        }
+        data={[]} 
+        renderItem={null}
+        keyExtractor={() => "dummy"} 
       />
-      <InfoCardSection />
-      <SensorStatus />
-      <Timeline />
-      </ScrollView>
     </SafeAreaView>
   );
 };
