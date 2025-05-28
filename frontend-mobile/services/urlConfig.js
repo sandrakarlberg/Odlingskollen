e0b407de50d9725ad676f4ffda4a3759ff93e807
sandrakarlberg <sandra.karlberg@hotmail.com>
2025-05-28 09:37:09 +0200

    Changed baseUrl

--- frontend-mobile/services/urlConfig.js
+++ frontend-mobile/services/urlConfig.js
@@ -1,13 +1,3 @@
-import { Platform } from "react-native";
-
-let baseUrl = "";
-
-if (Platform.OS === "android") {
-  baseUrl = "http://10.0.2.2:3000";
-} else if (Platform.OS === "web") {
-  baseUrl = "http://localhost:3000";
-} else {
-  baseUrl = "http://192.168.68.105:3000"; //Byt ut mot egen IP-adress
-}
+let baseUrl = "https://app-arts-chas-challenge.vercel.app";
 
 export default baseUrl;
